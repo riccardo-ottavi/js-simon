aedba067dc1fed96e7a242438b26d432ff0e26f5
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-08 14:58:26 +0200

    scrivi funzione responsabile di generare i numeri

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 //genero i numeri casuali
 
+
+let myArray = generateRandom(5, 0, 50);
+console.log(myArray);
+
+function generateRandom(n, min, max) {
+    let randomNumbs = [];
+    for (let i = 0; i < n; i++) {
+        randomNumbs[i] = Math.floor((Math.random() * max) +1 );
+    }
+    return randomNumbs;
+}
+
 //verifico che i numeri non si ripetino
 
 //faccio partire il timer di 30 secondi e  visualizzo in pagina i numeri da ricordare
