782e64dbaba59d0968023a3c843bc5c0f269ae3d
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-08 15:32:03 +0200

    visualizza i numeri generati in pagina

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
+//acquisisco gli elementi in pagina
+let myCount = document.getElementById("countdown");
+
+//acquisisco out
+let myDisplay = document.getElementById("numbers-list");
+
 //genero i numeri casuali
 //verifico che i numeri non si ripetino
-let myArray = generateRandom(5, 0, 50);
+let myArray = generateRandom(5, 1, 50);
 console.log(myArray);
 
 
-
-
-
-
 //faccio partire il timer di 30 secondi e  visualizzo in pagina i numeri da ricordare
+for (let i = 0 ; i < myArray.length; i++) {
+    const li = document.createElement("li");
+    li.innerHTML = myArray[i];
+    myDisplay.appendChild(li);
+    }
+
 
 // i numeri generati spariscono
 
@@ -17,15 +25,6 @@ console.log(myArray);
 //confronto i numeri generati con quelli inseriti dall'utente
 
 // out: quali e quanti numeri indovinati 
-
-
-
-
-
-
-
-
-
 
 
 
@@ -40,4 +39,5 @@ function generateRandom(n, min, max) {
     }
     return randomNumbs;
 }
+
 
