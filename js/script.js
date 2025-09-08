a77dbb62f951906adbdafdd08002d72c0447f599
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-08 14:44:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//genero i numeri casuali
+
+//verifico che i numeri non si ripetino
+
+//faccio partire il timer di 30 secondi e  visualizzo in pagina i numeri da ricordare
+
+// i numeri generati spariscono
+
+//acquisisco i numeri inseriti dall'utente
+
+//confronto i numeri generati con quelli inseriti dall'utente
+
+// out: quali e quanti numeri indovinati 
